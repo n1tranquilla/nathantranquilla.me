fd2102a53782806e9439e13a2ece8c41b961183b
Nathan Tranquilla <n1tranquilla@gmail.com>
2020-04-08 20:45:18 -0300

    fixed media query

--- src/webpack/assets/js/index.js
+++ src/webpack/assets/js/index.js
@@ -2,17 +2,20 @@ import 'lean-blog-web-components/src/app-bar';
 
 window.onload = function(){
     const mq = window.matchMedia('(max-width: 475px)');
-    const appbar = document.querySelector('lbwc-app-bar')
-    mq.addListener(function(changed) {
-        
-        if(changed.matches && !appbar.hasAttribute('mobile')) {    
+    const appbar = document.querySelector('lbwc-app-bar');
+
+    const handleMediaQuery = (mediaQuery) => {
+        if(mediaQuery.matches && !appbar.hasAttribute('mobile')) {    
             appbar.setAttribute('mobile','');
         }
         
-        if(!changed.matches && appbar.hasAttribute('mobile')) {
+        if(!mediaQuery.matches && appbar.hasAttribute('mobile')) {
             appbar.removeAttribute('mobile')
         }
-    });
+    }
+
+    handleMediaQuery(mq);
+    mq.addListener(handleMediaQuery);
 }
 
 
