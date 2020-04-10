379ffb612a3f384fe1b609afb94cb09a09e79563
Nathan Tranquilla <n1tranquilla@gmail.com>
2020-04-10 12:33:42 -0300

    add lazy loading images

--- src/webpack/assets/js/deferable.js
+++ src/webpack/assets/js/deferable.js
@@ -2,3 +2,4 @@
 import 'lean-blog-web-components/src/article';
 import 'lean-blog-web-components/src/index-page';
 import 'lean-blog-web-components/src/a'
+import 'lean-blog-web-components/src/img-block'
